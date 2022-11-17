80f3466910a7a8e8db4d1377477b0a8594f2c5d9
Cleber Neitzke <66871446+clneitzke@users.noreply.github.com>
2022-11-17 09:44:59 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "Extensions",
+    name: "swift-ios-extensions",
     platforms: [.iOS(.v10), ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Extensions",
-            targets: ["Extensions"]),
+            name: "swift-ios-extensions",
+            targets: ["swift-ios-extensions"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Extensions",
+            name: "swift-ios-extensions",
             dependencies: []),
         .testTarget(
-            name: "ExtensionsTests",
-            dependencies: ["Extensions"]),
+            name: "swift-ios-extensions-test",
+            dependencies: ["swift-ios-extensions"]),
     ]
 )
