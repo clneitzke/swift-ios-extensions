9a0d456f70b8927c5aa411534002e322daff1b2a
Cleber Neitzke <clneitzke@gmail.com>
2023-06-07 15:16:30 -0700

    Add htmlAttributedString

--- MyExtensionPackage/Extensions/String.swift
+++ MyExtensionPackage/Extensions/String.swift
@@ -5,7 +5,12 @@
 //  Created by clneitzke on 10/01/22.
 //
 
+
 import Foundation
+
+#if os(iOS)
+import UIKit
+#endif
 
 public extension String {
     func trim() -> String {
@@ -16,5 +21,45 @@ public extension String {
         let components = self.components(separatedBy: .whitespacesAndNewlines)
         return components.filter { !$0.isEmpty }.joined(separator: " ")
     }
+    
+#if os(iOS)
+    func htmlAttributedString() -> NSAttributedString? {
+        // use in label.attributedText
+        
+        let htmlTemplate = """
+        <!doctype html>
+        <html lang="pt-br">
+          <head>
+            <style>
+              body {
+                font-family: -apple-system;
+                font-size: 14px;
+              }
+            </style>
+          </head>
+          <body>
+            \(self)
+          </body>
+        </html>
+        """
+//        guard let data = htmlTemplate.data(using: .utf8) else {
+        guard let data = htmlTemplate.data(using: .isoLatin1) else {
+            return nil
+        }
+
+        
+        guard let attributedString = try? NSAttributedString(
+            data: data,
+            options: [.documentType: NSAttributedString.DocumentType.html],
+            documentAttributes: nil
+            ) else {
+            return nil
+        }
+
+        return attributedString
+        
+    }
+#endif
 }
+
 
